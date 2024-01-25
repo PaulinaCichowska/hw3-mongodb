b43d3c135f6c6f9cc971ff252ce50a25965c4ceb
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2024-01-25 14:29:27 +0100

    add dotenv

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from 'express'
-import logger from "morgan"
 import cors from "cors"
 import router from '#routes/api/contacts.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
 
 
 const app = express()
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
+// app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
